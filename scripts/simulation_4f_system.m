2974c16a79497a29e1e6cd79cf3c4b11edfbd4a2
Asaf90 <58466785+Asaf90@users.noreply.github.com>
2020-12-04 18:17:47 +0200

    added image simulation for out of focus planes

--- scripts/simulation_4f_system.m
+++ scripts/simulation_4f_system.m
@@ -6,6 +6,7 @@ N = 1000;                   % resolution
 lambda = 0.633e-6;          % wavelength
 SLM_pixel = 8e-6;           % SLM pixel resolution
 del = SLM_pixel;            % SLM pixel resolution - AGAIN!
+delta_z = 10e-3;            % distance to move the image plane out of focus
 x = -N/2:N/2-1;             % x axis span
 y = -N/2:N/2-1;             % y axis span
 [X,Y] = meshgrid(x*del,y*del);  %create x,y meshgrid
@@ -21,22 +22,28 @@ A = circshift(A,[450 450]); % center image elements
 P = exp(1i*double(A));      % convert A to double and vreate phase object
 figure;
 title('USAF Resolution chart as phase object')
-imagesc(abs(P))                  % show phase
+imagesc(abs(P))                  % show phase object
 %%  Phase functions
 f1 = 100e-3;                % focus length of first lens
 f2 = 100e-3;                % focus length of second lens
 z_o = 100e-3;               % distance from object plane to first lens
 z_i = 100e-3;               % distance from second lebs to image plane
-%% Complex Field Propagation of A                    
-A_image_plane = propagation4f(A,[z_o z_i],[f1 f2], lambda, X,Y);
 %% Complex Field Propagation of P
 P_image_plane = propagation4f(P,[z_o z_i],[f1 f2], lambda, X,Y);
-%% Camera
-I_amp = A_image_plane.*conj(A_image_plane);         % intensity of the image at imaging plane I = u*(u*)
-I_phase = P_image_plane.*conj(P_image_plane);
+P_after_image_plane = propagation4f(P,[z_o z_i+delta_z],[f1 f2], lambda, X,Y);
+P_before_image_plane = propagation4f(P,[z_o z_i-delta_z],[f1 f2], lambda, X,Y);
+%% Camera        
+I_phase = P_image_plane.*conj(P_image_plane);   % intensity of the image at imaging plane I = u*(u*)
 figure;
-imagesc(I_amp)                               % show intensity of normal object
-title('Intensity of normal object')
+imagesc(I_phase)                                % show intensity of phase object1
+title('Intensity of phase object at image plane')
+
+I_phase_after = P_after_image_plane.*conj(P_after_image_plane);   % intensity of the image at imaging plane I = u*(u*)
 figure;
-imagesc(I_phase)                               % show intensity of phase object1
-title('Intensity of phase object')
+imagesc(I_phase_after)                                % show intensity of phase object1
+title('Intensity of phase object after image plane')
+
+I_phase_before = P_before_image_plane.*conj(P_before_image_plane);   % intensity of the image at imaging plane I = u*(u*)
+figure;
+imagesc(I_phase_before)                                % show intensity of phase object1
+title('Intensity of phase object before image plane')
