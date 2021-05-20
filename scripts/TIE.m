ec2cfdb987a58d0f9f478a375db15806b36c261d
Asaf Cohen <asaf1990@gmail.com>
2021-05-20 20:39:12 +0300

    reconstructed the image!!!!!
    reconstructed the image!!!!!

--- scripts/TIE.m
+++ scripts/TIE.m
@@ -15,7 +15,7 @@ if SLM_type == 1
     I = -k_0*dIdz./I_avg;                              % Laplacian
 else
     resolution = length(I_image);
-    margin = 100;
+    margin = 80;
     I_left = I_image(479 - margin:541 + margin, 394 - margin:444 + margin);                        % left part of image
     I_right = I_image(479 - margin:541 + margin, 569 - margin:619 + margin);                  %right part of image
     figure;
@@ -28,9 +28,7 @@ else
     I_avg = mean(mean(I_image(100:900,100:900)));       % avarage value of intensity at image plane
     I = -k_0*dIdz./I_avg;                              % Laplacian
     I(resolution,resolution) = 0;
-    h_move = fix((resolution - (size(dIdz,1) + 1))/2);  % pixels to shift height for centering
-    w_move = fix((resolution - (size(dIdz,2) + 1))/2);  % pixels to shift width for centering
-    I = circshift(I,[h_move w_move]);     % center image elements
+    I = centerImage(I,size(dIdz));
     figure;
     imagesc(I)
     title('dIdz with SLM')
@@ -54,30 +52,6 @@ I_abs = abs(I);
 comp_abs = abs(compare);
 I_norm = I./max(I_abs(:));
 comp_norm = compare./max(comp_abs(:));
-
-% adding artificial threshold to filter laplacian noise
-laplace_filter_upper = 5e10;
-laplace_filter_lower = 5.933e10;
-I_thresh = zeros(size(I)) + (I > laplace_filter_upper).*I + (I < -laplace_filter_lower).*I;
-I_thresh_norm = zeros(size(I)) + (I_norm > 0.3).*I_norm + (I_norm < -0.3).*I_norm;
-
-% adding avaraging to filtered image
-avg_filter_pos_upper = 3.14e10;
-avg_filter_pos_lower = 1.372e10;
-avg_filter_neg_upper = -1.46e10;
-avg_filter_neg_lower = -3.071e10;
-
-pos_avg_mat = (I_thresh > avg_filter_pos_lower).*(I_thresh < avg_filter_pos_upper).*I_thresh;
-neg_avg_mat = (I_thresh > avg_filter_neg_lower).*(I_thresh < avg_filter_neg_upper).*I_thresh;
-pos_nozeros = pos_avg_mat ~= 0;
-neg_nozeros = neg_avg_mat ~= 0;
-
-pos_avg = mean(pos_avg_mat(pos_nozeros));
-neg_avg = mean(neg_avg_mat(neg_nozeros));
-
-I_thresh(pos_nozeros) = pos_avg;
-I_thresh(neg_nozeros) = neg_avg;
-
 
 if graphs
    
@@ -119,7 +93,6 @@ if graphs
 
 end
 
-% k_recip = 1./(k_x.^2 + k_y.^2);% reciprocal of sum of spatial freq's
 
 % recipcoral sum of spatial freq's using nati's code:
 
@@ -140,41 +113,16 @@ end
 rec_phase = ift2(k_recip .* ft2(I));                 % reconstructing phase
 image_data = real(rec_phase);                       % get real part of reconstructed phase
 
-% rec_phase_comp = ift2(k_recip .* ft2(compare));
-% image_data_comp = real(rec_phase_comp);
-% 
-% rec_phase_from_thresh = ift2(k_recip .* ft2(I_thresh));                 % reconstructing phase with artificial threshold
-% image_data_thresh = real(rec_phase_from_thresh);                       % get real part of reconstructed phase
-% 
-% rec_phase_from_thresh_norm = ift2(k_recip .* ft2(I_thresh_norm));                 % reconstructing phase with arti. threshold and norm.
-% image_data_thresh_norm = real(rec_phase_from_thresh_norm);                       % get real part of reconstructed phase
 
-% analyze reconstructed phase
-% I_hist = histogram(image_data(350:440, 360:440));
 slice = image_data(360:440, 400);
-% I_bin_idx_above = I_hist >= 1.77e12;
-% I_bin_idx_below = I_hist < 1.77e12;
-% I_bin = I_hist.*I_bin_idx_above;
-% I_bin_not = I_hist.*I_bin_idx_below;
-% 
-% bin_mean = mean(mean(I_bin));
-% bin_mean_not = mean(mean(I_bin_not));
+
 
 if ~graphs                           % graphs of reconstructed image
     
     figure;
     plot(slice)
 
-%     figure;
-%     imagesc(I_bin)
-%     colorbar
-%     title('binary')
-%     
-%     figure;
-%     imagesc(I_bin_not)
-%     colorbar
-%     title('binary not')
-%     
+
     figureToSave = figure;
     imagesc(image_data);
     colorbar();
--- scripts/simulation_4f_system.m
+++ scripts/simulation_4f_system.m
@@ -20,14 +20,14 @@ h_end = 860;                        % height end point for cropping
 w_start = 30;                       % width starting point for cropping
 w_end = 100;                         % width end point for cropping
 resize_factor = 1;                 % resize factor for scaling image
-h_move = fix((resolution - (h_end - h_start + 1)*resize_factor)/2);  % pixels to shift height for centering
-w_move = fix((resolution - (w_end - w_start + 1)*resize_factor)/2);  % pixels to shift width for centering
+
 
 threshold = 240;                                    % threshold for binary phase usage
 cropped_img = image(h_start:h_end,w_start:w_end);   % select specified rows and columns from image
 cropped_img = imresize(cropped_img, resize_factor);
+cropped_img_size = size(cropped_img);
 cropped_img(resolution,resolution) = 0;             % increase image size to 1000x1000
-cropped_img = circshift(cropped_img,[h_move w_move]);     % center image elements
+cropped_img = centerImage(cropped_img, cropped_img_size);
 binary_img = uint8((cropped_img >= threshold));     % create binary values depending on threshold
 
 if graphs
@@ -41,6 +41,14 @@ end
 
 phase_const = pi/3;                                  % constant to multiply binary img, s.t. exp doesnt zero
 phase_obj = complex(exp(1i*double(binary_img)*phase_const));      % convert A to double and create phase object
+
+mask = ones(126,113);                               % create a mask to remove noise in TIE
+mask_size = size(mask);
+mask(resolution,resolution) = 0;
+mask = centerImage(mask, mask_size);
+
+phase_obj = phase_obj.*mask;                        % mask phase object to clear shift noise
+
 if graphs
     figureToSave = figure;
     imagesc(angle(phase_obj))          % show phase object
